c7d1e7812030f7c8cb80e174000bab8e45c27056
Jacob Adelman <jacobzadelman@gmail.com>
2022-10-06 21:09:50 -0400

    add res render homepage and login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.render('homepage')
+})
+
 router.get("/login", (req, res) => {
   console.log(req.session);
   if (req.session.loggedIn) {
     console.log("logged in")
-    res.redirect("/dashboard.html");
+    res.render('dashboard');
     return;
   }
 
-  res.redirect("/login.html");
+  res.render('login')
 });
 
 module.exports = router;

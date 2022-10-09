6b94af2e36ba8884f3842cba8bdfa414ab92383c
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-08 19:08:31 -0500

    routes for users revised, added email to User model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,111 +1,156 @@
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
+const router = require('express').Router();
+const { User, Attendence, Comment, Post } = require('../../models');
 
-//Requiring the data
-const { users } = require('users.json'); //Ask loction of source file
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-
-//Query function for GET method
-function filterByQuery(query, usersArray) {
-  let filteredResults = usersArray;
-
-  if (query.id) {
-    filteredResults = filteredResults.filter(user => user.id === query.id);
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(user => user.name === query.name);
-  }
-  if (query.email) {
-    filteredResults = filteredResults.filter(user => user.email === query.email);
-  }
-  //verify passwords route
-  if (query.password) {
-    filteredResults = filteredResults.filter(user => user.password === query.password);
-  }
-  return filteredResults;
-}
-
-//Filter by Name function for GET method
-function findById(id, usersArray) {
-  const result = usersArray.filter(user => iser.id === id)[0];
-  return result;
-}
-
-//Function to add user to json file using POST method
-function createUser (body, usersArray) {
-  console.log(body);
-
-  const user = body;
-  usersArray.push(user);
-  fs.writeFileSync(
-
-    path.join(__dirname, 'users.json'), //Ask loction of file
-    JSON.stringify({ users: usersArray }, null, 2)
-  );
-  return user;
-}
-
-//Function to validate user's data from body
-function validateUser(user) {
-  if (!user.name || typeof user.name !== 'string') {
-    return false;
-  }
-  if (!user.password || typeof user.password !== 'string') {
-    return false;
-  }
-  if (!user.email || typeof user.email !== 'string') {
-    return false;
-  }
-  return true;
-}
-
-//Get method to filter by query
-app.get('/api/users', (req, res) => {
-  let results = users;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  console.log(req.query);
-  res.json(results);
+//Route to get all users
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-//Get method to filter by ID
-app.get('/api/users/:id', (request, response) => {
-  const result =findById(request.params.id, users);
-  if (result) {
-    response.json(result);
-  } else {
-    response.send(404);
+//Route to get one user by ID
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.user_id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['post_id', 'title', 'contents', 'date_created']
+      },
+      {
+        model: Comment,
+        attributes: ['comment_id', 'content', 'post_id', 'date_created'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+    ]
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'The user could not be found' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Route to create users
+router.post('/', (req, res) => {
+  User.create({
+    user_name: req.body.user_name,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.user_id;
+        req.session.user_name = dbUserData.user_name;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Route to login
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      user_name: req.body.user_name
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "Sorry, we don't recognize this user name" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'The password is incorrect' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.user_id;
+      req.session.user_name = dbUserData.user_name;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: 'Logged in successfully!' });
+    });
+  });
+});
+
+//Route to log out
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
   }
 });
 
-//POST method to add users to json file
-app.post('/api/users', (req, res) => {
-  console.log(req.body);
-  req.body.id = users.length.toString();
-
-  if (!validateUser(req.body)) {
-    res.status(400).send('Please introduce all the required information.');
-  } else {
-    const user = createUser(req.body, users);
-
-    //Converts response to JSON format
-    res.json(req.body);
-  }
+//Update user data
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.user_id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-//Getting html to be served from Express.js server
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './index.html')); //Ask location of html
+//Delete user by id
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.user_id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+module.exports = router;
--- models/User.js
+++ models/User.js
@@ -15,11 +15,18 @@ User.init(
     user_name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
+    }
   },
   {
     hooks: {

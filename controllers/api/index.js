7b9aedf0b67c5fd97c2070ff7034f92615f108af
Bawandeep Singh <Singhbawan48@gmail.com>
2022-10-09 14:14:14 -0400

    fixed attendence model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
+//route until here  /api/
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);

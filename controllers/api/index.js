2cf93990b5e093b5f6023935427f2fbe6d87fd6b
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-08 22:43:43 -0500

    added exports router to attendance route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
+const postRoutes = require('../home-routes');
 const commentRoutes = require('./comment-routes');
 const attendanceRoutes = require('./attendance-routes');
 

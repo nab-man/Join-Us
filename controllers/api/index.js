adc0e4a5dfc367030e2d8e265ccbccf34760cd8e
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-08 19:28:20 -0500

    set up the server in index files from api and controllers directory

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const attendanceRoutes = require('./attendance-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/attendance', attendanceRoutes);
 
 module.exports = router;

b6d9a231dbdb4dec968a32568a6296958eb4dcde
Jacob Adelman <jacobzadelman@gmail.com>
2022-10-12 00:38:24 -0400

    change config.js

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -1,6 +1,6 @@
 function getUrl() {
     if (window.location.href.includes("join-us")) {
-        return "http://join-us2.herokuapp.com"
+        return "https://join-us2.herokuapp.com"
     } else {
         return "http://localhost:3000"
     }
